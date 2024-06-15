82953b21b2aad928d25ed5b56e6eed09d4e0cdf9
Riley Lane <rileylanea@gmail.com>
2024-06-15 13:27:54 -0700

    Fine-tune driving controls. Create ferris wheel

---
+++ ferris_wheel.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+var time = 0;
+var distance = 50;
+var timeScale = 0.03;
+@export var offset = 0.0;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.z = distance * sin(time * timeScale + offset * PI);
+	position.y = distance * cos(time * timeScale + offset * PI);
+	time += delta;
--- segway.gd
+++ segway.gd
@@ -1,14 +1,14 @@
 extends VehicleBody3D
 
-var horsepower = 2000
+var horsepower = 1600
 
-var steer_limit = deg_to_rad(45)
+var steer_limit = deg_to_rad(35)
 
 @onready var steeringStickModel = $CSGCombiner3D/SteeringStick;
 @onready var raycast3D = $RayCast3D;
 
 func _physics_process(delta):
-	var move_input = Input.get_action_strength("accelerate") - Input.get_action_strength("reverse")
+	var move_input = Input.get_action_strength("accelerate") - Input.get_action_strength("reverse") / 1.5
 	engine_force = lerpf(engine_force, move_input * horsepower, delta * 10);
 
 	var turn_input = Input.get_action_strength("steer_left") -  Input.get_action_strength("steer_right")
