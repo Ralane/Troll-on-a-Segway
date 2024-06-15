656a5f2572ad22f3020c57da23d452535882579a
Riley Lane <rileylanea@gmail.com>
2024-06-14 22:32:05 -0700

    Animation on steer

--- segway.gd
+++ segway.gd
@@ -4,9 +4,13 @@ var horsepower = 2000
 
 var steer_limit = deg_to_rad(45)
 
+@onready var steeringStickModel = $CSGCombiner3D/SteeringStick;
+
 func _physics_process(delta):
 	var move_input = Input.get_action_strength("accelerate")
 	engine_force = lerpf(engine_force, move_input * horsepower, delta * 10);
 
 	var turn_input = Input.get_action_strength("steer_left") -  Input.get_action_strength("steer_right")
-	steering = lerpf(steering, turn_input * steer_limit, delta * 5);
+	steering = lerpf(steering, turn_input * steer_limit, delta * 10);
+
+	steeringStickModel.rotation_degrees.y = steering * 45	
