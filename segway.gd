636f3a8eda06c0dac48cfdc706b0b5b6c9c29e72
Riley Lane <rileylanea@gmail.com>
2024-06-14 22:25:36 -0700

    WIP vehicle physics

--- segway.gd
+++ segway.gd
@@ -2,11 +2,11 @@ extends VehicleBody3D
 
 var horsepower = 2000
 
-var steer_limit = deg_to_rad(30)
+var steer_limit = deg_to_rad(45)
 
 func _physics_process(delta):
 	var move_input = Input.get_action_strength("accelerate")
-	engine_force = move_input * horsepower
+	engine_force = lerpf(engine_force, move_input * horsepower, delta * 10);
 
 	var turn_input = Input.get_action_strength("steer_left") -  Input.get_action_strength("steer_right")
-	steering = turn_input * steer_limit
+	steering = lerpf(steering, turn_input * steer_limit, delta * 5);
