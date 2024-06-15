a6729eb8bceb712120dfd7aa526391dac60dcd9f
Riley Lane <rileylanea@gmail.com>
2024-06-14 19:03:12 -0700

    WIP

--- segway.gd
+++ segway.gd
@@ -1,11 +1,11 @@
 extends VehicleBody3D
 
-var horsepower = 1000
+var horsepower = 2000
 
 var steer_limit = deg_to_rad(30)
 
 func _physics_process(delta):
-	var move_input = Input.get_action_strength("accelerate") -  Input.get_action_strength("reverse")
+	var move_input = Input.get_action_strength("accelerate")
 	engine_force = move_input * horsepower
 
 	var turn_input = Input.get_action_strength("steer_left") -  Input.get_action_strength("steer_right")
