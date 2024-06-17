f6ad4b5f24eea0a87dd72ed2b6a97dae9a4544c2
Riley Lane <rileylanea@gmail.com>
2024-06-17 13:46:43 -0700

    Dwarven caverns sounds

--- garden_gnome_4k.gd
+++ garden_gnome_4k.gd
@@ -5,7 +5,7 @@ extends RigidBody3D
 func _on_body_entered(body):
 	if(!impactSound.playing):
 		if(body != null and "linear_velocity" in body):
-			impactSound.volume_db = -25 + absf(linear_velocity.length() - body.linear_velocity.length());
+			impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
 		else:
 			impactSound.volume_db = -30 + absf(linear_velocity.length());
 		impactSound.play();
@@ -14,7 +14,7 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if(!impactSound.playing):
 		if(body != null and "linear_velocity" in body):
-			impactSound.volume_db = -25 + absf(linear_velocity.length() - body.linear_velocity.length());
+			impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
 		else:
 			impactSound.volume_db = -30 + absf(linear_velocity.length());
 		impactSound.play();
