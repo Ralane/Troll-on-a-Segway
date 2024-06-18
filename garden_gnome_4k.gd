769709cdec5a9ed60510470a5d3a6dc39569e158
Riley Lane <rileylanea@gmail.com>
2024-06-18 14:53:57 -0700

    Avoid playing gnome sounds on mobile to avoid crackling

--- garden_gnome_4k.gd
+++ garden_gnome_4k.gd
@@ -3,19 +3,21 @@ extends RigidBody3D
 @onready var impactSound = $ImpactSound;
 
 func _on_body_entered(body):
-	if(!impactSound.playing):
-		if(body != null and "linear_velocity" in body):
-			impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
-		else:
-			impactSound.volume_db = -30 + absf(linear_velocity.length());
-		impactSound.play();
+	if(!OS.has_feature("mobile")): # Avoid playing on mobile due to audio crackling
+		if(!impactSound.playing):
+			if(body != null and "linear_velocity" in body):
+				impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
+			else:
+				impactSound.volume_db = -30 + absf(linear_velocity.length());
+			impactSound.play();
 
 
 func _on_body_exited(body):
-	if(!impactSound.playing):
-		if(body != null and "linear_velocity" in body):
-			impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
-		else:
-			impactSound.volume_db = -30 + absf(linear_velocity.length());
-		impactSound.play();
+	if(!OS.has_feature("mobile")): # Avoid playing on mobile due to audio crackling
+		if(!impactSound.playing):
+			if(body != null and "linear_velocity" in body):
+				impactSound.volume_db = -20 + absf(linear_velocity.length() - body.linear_velocity.length());
+			else:
+				impactSound.volume_db = -30 + absf(linear_velocity.length());
+			impactSound.play();
 
